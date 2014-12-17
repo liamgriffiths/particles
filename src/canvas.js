e6abc9e3d364bb82f50a9b1684cb11794a5ad1be
Liam Griffiths <liamgriffiths@gmail.com>
2014-12-17 01:46:48 -0500

    fix typo

--- src/canvas.js
+++ src/canvas.js
@@ -36,7 +36,7 @@ var Canvas = React.createClass({
   },
 
   handleTouchStart(event) {
-    event.PreventDefault();
+    event.preventDefault();
     this.setTouch(event);
     this.touching = setInterval(_ => {
       this.animation.particleController.add(this.getTouch());
@@ -44,13 +44,13 @@ var Canvas = React.createClass({
   },
 
   handleTouchMove(event) {
-    event.PreventDefault();
+    event.preventDefault();
     this.setTouch(event);
     this.animation.particleController.add(this.getTouch());
   },
 
   handleTouchEnd(event) {
-    event.PreventDefault();
+    event.preventDefault();
     clearInterval(this.touching);
   },
 
