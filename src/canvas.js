1807ff9fb9cb09b843364e7a5291cc99c421d28e
Liam Griffiths <liamgriffiths@gmail.com>
2014-12-16 21:21:17 -0500

    getting started!

---
+++ src/canvas.js
@@ -0,0 +1,10 @@
+var React = require('react');
+
+var Canvas = React.createClass({
+
+  render: function() {
+    return <h1>hello!</h1>;
+  }
+});
+
+module.exports = Canvas;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+var React = require('react');
+var Canvas = require('./canvas');
+
+var root = document.querySelector('body');
+React.render(<Canvas />, root);
