35beb5b32fd4b16fd23ea0c7cb96f80e450616b1
Liam Griffiths <liamgriffiths@gmail.com>
2014-12-17 23:09:45 -0500

    add index, server, update package.json

---
+++ server.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var fs = require('fs');
+
+var assets = {
+  '/': __dirname + '/index.html',
+  '/bundle.js': __dirname + '/build/bundle.js'
+};
+
+var port = process.env.PORT || 3000;
+
+var server = http.createServer(function(req, res) {
+  if (req.url in assets) {
+    fs.createReadStream(assets[req.url]).pipe(res);
+  } else {
+    res.end();
+  }
+});
+
+server.listen(port, function() {
+  console.log('listening on', port);
+});
