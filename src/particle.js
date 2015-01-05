9e4c8fc2b383f31b9f7c7fc0e0b667cb73ffa4b5
Liam Griffiths <liamgriffiths@gmail.com>
2015-01-04 22:43:57 -0500

    clean up particle

--- src/particle.js
+++ src/particle.js
@@ -1,10 +1,12 @@
-var {randFloat, createGradient, Map} = require('./helpers');
+var {randFloat, getTime, HashMap} = require('./helpers');
 var SimplexNoise = require('simplex-noise');
 var simplex = new SimplexNoise(Math.random);
 
 var PRECISION = 1;
 var MAX_SIZE = 25;
 var MIN_SIZE = Math.pow(10, -PRECISION);
+
+var WHITE = '#ffffff';
 
 var Particle = function(ctx, opts) {
   this.ctx = ctx;
@@ -21,7 +23,16 @@ var Particle = function(ctx, opts) {
   this.color = opts.color;
 };
 
-Particle.images = new Map();
+Particle.images = new HashMap();
+
+Particle.createRadialGradient = (ctx, x, y, size, color) => {
+  var gradient = ctx.createRadialGradient(x, y, 0, x, y, size);
+  gradient.addColorStop(0.0, WHITE);
+  gradient.addColorStop(0.5, WHITE);
+  gradient.addColorStop(1.0, color);
+  return gradient;
+}
+
 Particle.preRender = (color) => {
   for (var i = MIN_SIZE; i <= MAX_SIZE; i += Math.pow(10, -PRECISION)) {
     var size = +i.toFixed(PRECISION);
@@ -34,7 +45,7 @@ Particle.preRender = (color) => {
     var y = canvas.height / 2;
 
     context.beginPath();
-    context.fillStyle = createGradient(context, x, y, size, color);
+    context.fillStyle = Particle.createRadialGradient(context, x, y, size, color);
     context.arc(x, y, size, 0, 2 * Math.PI, false);
     context.fill();
     context.closePath();
@@ -51,7 +62,7 @@ Particle.prototype = {
   update() {
     this.ageRatio = (1 - (this.age / this.lifespan));
 
-    var noise = simplex.noise3D(this.position.x, this.position.y, _GET_ELAPSED_TIME());
+    var noise = simplex.noise3D(this.position.x, this.position.y, getTime());
 
     this.direction.x += noise * randFloat(0.1, 0.5);
     this.direction.y += noise * randFloat(0.1, 0.5);
